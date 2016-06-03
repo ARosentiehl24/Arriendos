f17dab36cc0d7e341b331d037352f7f3ecd90a54
AlandEr2612 <AlandEr.0301>
2016-06-03 17:41:01 -0500

    Update

---
+++ app/src/main/java/com/unimagdalena/edu/co/arriendos/Constants.java
@@ -0,0 +1,19 @@
+package com.unimagdalena.edu.co.arriendos;
+
+public class Constants {
+    public static final int DETAIL_CODE = 100;
+
+    public static final int UPDATE_CODE = 101;
+
+    private static final String PORT = "63343";
+
+    private static final String IP = "http://10.0.2.2:";
+
+    public static final String GET = IP + PORT + "/I%20Wish/obtener_metas.php";
+    public static final String GET_BY_ID = IP + PORT + "/I%20Wish/obtener_meta_por_id.php";
+    public static final String INSERT = IP + PORT + "/I%20Wish/insertar_meta.php";
+    public static final String UPDATE = IP + PORT + "/I%20Wish/actualizar_meta.php";
+    public static final String DELETE = IP + PORT + "/I%20Wish/borrar_meta.php";
+
+    public static final String EXTRA_ID = "IDEXTRA";
+}
--- app/src/main/java/com/unimagdalena/edu/co/arriendos/MainActivity.java
+++ app/src/main/java/com/unimagdalena/edu/co/arriendos/MainActivity.java
@@ -21,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+
     }
 
     @Override
